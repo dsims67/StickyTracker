0da08e558a749eea723798d22a12128e9a1fb19a
XRECKXIMPACT <nick.degruccio@gmail.com>
2016-02-28 16:21:17 -0700

    Added files via upload

--- StickyTracker/src/main/java/com/aetheriumwars/stickytracker/listeners/OnHitWithTracker.java
+++ StickyTracker/src/main/java/com/aetheriumwars/stickytracker/listeners/OnHitWithTracker.java
@@ -14,29 +14,29 @@ import com.aetheriumwars.stickytracker.tracker.Tracker;
 import net.md_5.bungee.api.ChatColor;
 
 public class OnHitWithTracker implements Listener{
-	
-	@EventHandler
-	public void onPlayerDamage(EntityDamageByEntityEvent ev) {
-		if(ev.getEntity() instanceof Player && ev.getDamager() instanceof Player) {
-			Player tracker = (Player) ev.getDamager();
-			Player trackee = (Player) ev.getEntity();
-			Bukkit.getLogger().info("Material: "+tracker.getItemInHand().getType().toString()+" id: "+tracker.getItemInHand().getTypeId());
-			//if(tracker.getItemInHand().getType().equals(StickyTracker.trackerItem)) {
-			if(tracker.getItemInHand().getTypeId() == 151) {
-				if(StickyTracker.hasTracker(tracker)) {
-					StickyTracker.removeTracker(tracker);
-				}
-				
-				StickyTracker.addTracker(new Tracker(tracker, trackee));
-				if(tracker.getItemInHand().getAmount() > 1)
-					tracker.getItemInHand().setAmount(tracker.getItemInHand().getAmount()-1);
-				else {
-					tracker.setItemInHand(new ItemStack(Material.AIR));
-					tracker.updateInventory();
-				}
-				tracker.sendMessage(ChatColor.GREEN+"Tracker successfully attached to "+trackee.getDisplayName());
-			}
-		}
-	}
 
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent ev) {
+        if(ev.getEntity() instanceof Player && ev.getDamager() instanceof Player) {
+            Player tracker = (Player) ev.getDamager();
+            Player trackee = (Player) ev.getEntity();
+            Bukkit.getLogger().info("Material: "+tracker.getItemInHand().getType().toString()+" id: "+tracker.getItemInHand().getTypeId());
+            //if(tracker.getItemInHand().getType().equals(StickyTracker.trackerItem)) {
+
+            if(tracker.getItemInHand().getType().equals(StickyTracker.trackerItem)) {
+                if(StickyTracker.hasTracker(tracker)) {
+                    StickyTracker.removeTracker(tracker);
+                }
+
+                StickyTracker.addTracker(new Tracker(tracker, trackee));
+                if(tracker.getItemInHand().getAmount() > 1)
+                    tracker.getItemInHand().setAmount(tracker.getItemInHand().getAmount()-1);
+                else {
+                    tracker.setItemInHand(new ItemStack(Material.AIR));
+                    tracker.updateInventory();
+                }
+                tracker.sendMessage(ChatColor.GREEN+"Tracker successfully attached to "+trackee.getDisplayName());
+            }
+        }
+    }
 }
